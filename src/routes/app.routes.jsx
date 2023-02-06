60cbe3f80de18f91013ca2d4b8396884b8574bc9
Francisco <francar76@hotmail.com>
2023-02-06 08:24:06 -0300

    commit

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -3,12 +3,26 @@ import {Routes, Route } from 'react-router-dom'
 
 
 import { MyRequests } from '../pages/MyRequests'
+import { Home } from '../pages/Home'
+import { HomeAdmin } from '../pages/HomeAdmin'
+import { MyFavorites } from '../pages/MyFavorites'
+import { OrderHistory } from '../pages/OrderHistory'
+import { Plate } from '../pages/Plate'
+import { PlateAdmin } from '../pages/PlateAdmin'
+import { EditPlate } from '../pages/EditPlate'
 
 
 export function AppRoutes(){
   return (
     <Routes>
       <Route path="/myRequests" element={<MyRequests/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/homeAdmin" element={<HomeAdmin/>}/>
+      <Route path="/myFavorites" element={<MyFavorites/>}/>
+      <Route path="/orderHistory" element={<OrderHistory/>}/>
+      <Route path="/plate" element={<Plate/>}/>
+      <Route path="/plateAdmin" element={<PlateAdmin/>}/>
+      <Route path="/editPlate" element={<EditPlate/>}/>
     </Routes>
   )
 }
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -2,14 +2,13 @@ import {Routes, Route } from 'react-router-dom'
 
 
 import { SingIn } from '../pages/SingIn'
-import { SingTest } from '../pages/Test'
+
 import { SingUp } from '../pages/SingUp'
 
 
 export function AuthRoutes(){
   return (
     <Routes>
-      <Route path="/test" element={<SingTest/>}/>
       <Route path="/" element={<SingIn/>}/>
       <Route path="/register" element={<SingUp/>}/>
     </Routes>
