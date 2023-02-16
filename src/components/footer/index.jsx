fbe3e6e15c92c052b1135afda73f137ace1d7cd9
Francisco <francar76@hotmail.com>
2023-02-16 10:26:48 -0300

    commit

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,12 +6,12 @@ import  explorerLogo  from '../../assets/explorerLogo.svg'
 export function Footer() {
   return (
     <Container>
-      <div>
+      <div className='soon'>
         <img src={explorerLogo} alt="logo explorer" />
         <h1> food explorer</h1>
       </div>
 
-      <div>
+      <div className='copyrights'>
         <h2>© 2023 - Todos os direitos reservados.</h2>
       </div>
       
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -13,23 +13,47 @@ export const Container = styled.footer`
   padding: 2.4rem 12.3rem;
 
   background-color: ${({ theme }) => theme.COLORS.DARK_600};
-
-  > div {
-    display: flex;
-    gap: 1rem;
-    color: ${({ theme }) => theme.COLORS.LIGHT_700};
-    > h1 {
-      font-family: 'Roboto', sans-serif;
-      font-style: normal;
-      font-weight: 700;
-      font-size: 2.4rem;
-      line-height: 2.8rem;
+  @media (min-width: 64em) {
+    .soon {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      color: ${({ theme }) => theme.COLORS.LIGHT_700};
+      h1 {
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 2.4rem;
+        line-height: 2.8rem;
+      }
+    }
+    .copyrights {
+      > h2 {
+        font-size: 1.4rem;
+        color: ${({ theme }) => theme.COLORS.LIGHT_200};
+      }
     }
   }
-  > div:last-child {
-    > h2 {
-      font-size: 1.4rem;
-      color: ${({ theme }) => theme.COLORS.LIGHT_200};
+  @media (max-width: 64em) {
+    .soon {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      color: ${({ theme }) => theme.COLORS.LIGHT_700};
+      h1 {
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 1.5rem;
+        line-height: 1.7rem;
+      }
+    }
+    .copyrights {
+      > h2 {
+        font-size: 1.2rem;
+        line-height: 1.5rem;
+        color: ${({ theme }) => theme.COLORS.LIGHT_200};
+      }
     }
   }
 `
